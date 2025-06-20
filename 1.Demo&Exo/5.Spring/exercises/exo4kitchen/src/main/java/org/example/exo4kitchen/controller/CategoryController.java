9810908f47a6c325644d3eb0949d903d8055d7ac
tonyl59 <tony.luong59@yahoo.com>
2025-06-20 09:46:25 +0200

    continuation exo4 ( formulaire recipe fonctionne en mettant une valeur pour la valeur id int avant sinon crash car integer primitive null), manquerait à gérer la liste d'ingredients. Rajout d'element de correction (update delete)

--- 1.Demo&Exo/5.Spring/exercises/exo4kitchen/src/main/java/org/example/exo4kitchen/controller/CategoryController.java
+++ 1.Demo&Exo/5.Spring/exercises/exo4kitchen/src/main/java/org/example/exo4kitchen/controller/CategoryController.java
@@ -6,10 +6,7 @@ import org.example.exo4kitchen.service.CategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class CategoryController {
@@ -27,9 +24,12 @@ public class CategoryController {
     }
 
     @PostMapping("/addcategory")
-    public String submitCategory(@Valid @ModelAttribute("category") Category category, BindingResult bindingResult){ // Binding result & Valid allow to get message when null or not corresponding condition
+    public String submitCategory(@Valid @ModelAttribute("category") Category category, BindingResult bindingResult){ //Valid verifies that everything is correct in category
+        // Binding result will check if attributes are  valid with hasErrors, returning true if  category does not respect conditions, else false if its correct
+        // Binding result & Valid allow to get message when null or not corresponding condition
         if(bindingResult.hasErrors()){
-            return "categoryT/createpage"; // if i use redirect:/createcategory, it wont display the "error message null"
+            return "categoryT/createpage"; // if i use redirect:/createcategory, it wont display the "error message null".
+            // its because binding result will say that my object page has an error, redirect says that i finished my job... so it "throws everything" away.
         }
         categoryService.create(category);
         return "redirect:/createcategory";
@@ -48,5 +48,18 @@ public class CategoryController {
         Category category = categoryService.get_by_id(id);
         model.addAttribute(category);
         return "categoryT/detailpage";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") int id){
+        categoryService.delete(id);
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/update")
+    public String update(@RequestParam("id") int id, Model model){
+        Category category = categoryService.get_by_id(id);
+        model.addAttribute("category", category);
+        return "categoryT/createpage"; // reutilisation de la même page html
     }
 }
---
+++ 1.Demo&Exo/5.Spring/exercises/exo4kitchen/src/main/java/org/example/exo4kitchen/service/Repository.java
@@ -0,0 +1,6 @@
+package org.example.exo4kitchen.service;
+
+public interface Repository {
+
+
+}
