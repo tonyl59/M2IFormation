a5a83067129043fc39b9b3c89a967b106120613d
tonyl59 <tony.luong59@yahoo.com>
2025-07-28 12:23:50 +0200

    ajout de tailwind (j'ai mis le /* import tailwind */ sur styles.css pour garder mon css counter de base) + remis le check isPair dans le decrement que j'ai enlevé accidentellement

--- 6_Angular/exercices/exo1/exercice1/src/app/components/counter/counter.ts
+++ 6_Angular/exercices/exo1/exercice1/src/app/components/counter/counter.ts
@@ -19,13 +19,22 @@ export class Counter {
       this.isPair = false
     }
   }
-
   decrement() : void{ // pour décrementer le compteur
     if (this.count !=0){ // Permet d'indirectement mettre la limite minimum du compteur à 0 pour éviter qu'il soit au négatif
       this.count -= 1
     }
+
+        if (this.count%2 == 0){ // si nombre compteur est pair...
+      this.isPair = true
+    }
+    else{ // si nombre du compteur est impair...
+      this.isPair = false
+    }
+
+
     
   }
+  
 }
 
 
